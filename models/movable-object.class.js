c3a6da04004a623e53b862abc372e364a7cf91a5
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-06-10 15:11:32 +0200

    move charakter

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -5,6 +5,8 @@ class MovableObject {
   height = 150;
   width = 100;
   imageCache = [];
+  currentImage = 0;
+  speed = 0.15;
 
   loadImage(path) {
     this.img = new Image();
@@ -23,5 +25,9 @@ class MovableObject {
     console.log("Moving right");
   }
 
-  moveLeft() {}
+  moveLeft() {
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
+  }
 }
