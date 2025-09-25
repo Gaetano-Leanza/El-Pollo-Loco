3a958e64ab86621c929c8b49bfe547e33b8e1dae
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-09-25 23:08:29 +0200

    work on cloud.class.js

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,39 +1,70 @@
 /**
- * Represents a cloud object in the game background.
- * Inherits movement capabilities from MovableObject.
+ * Represents a cloud object that provides atmospheric background elements with parallax scrolling.
+ * Clouds move independently and respond to camera movement to create depth in the game world.
+ * @extends MovableObject
  */
 class Cloud extends MovableObject {
-  /** @type {number} Vertical position of the cloud */
+  /**
+   * Y-coordinate position of the cloud (high in the sky).
+   * @type {number}
+   * @default 20
+   */
   y = 20;
 
-  /** @type {number} Height of the cloud image */
-  height = 250;
+  /**
+   * Height of the cloud in pixels.
+   * @type {number}
+   * @default 300
+   */
+  height = 300;
 
-  /** @type {number} Width of the cloud image */
+  /**
+   * Width of the cloud in pixels.
+   * @type {number}
+   * @default 500
+   */
   width = 500;
 
   /**
-   * Creates a new Cloud instance.
-   * @param {number|null} [xPosition=null] - Optional X-position of the cloud. 
-   *   If null, a random X-position is assigned.
+   * Speed at which clouds move when following camera movement.
+   * Used for parallax scrolling effects.
+   * @type {number}
+   * @default 9
    */
-  constructor(xPosition = null) {
-    super().loadImage("../img/5_background/layers/4_clouds/1.png");
+  cameraSpeed = 9;
 
-    /**
-     * @type {number} Horizontal position of the cloud.
-     */
-    this.x = xPosition !== null ? xPosition : Math.random() * 500;
-
+  /**
+   * Creates a new Cloud instance with randomized position and movement speed.
+   * @param {string} path - The filename (without extension) of the cloud image to load from the clouds directory
+   */
+  constructor(path) {
+    super().loadImage("img/5_background/layers/4_clouds/" + path + ".png");
+    this.x = Math.random() * 2000;
+    this.speed = Math.random() * 0.17;
     this.animate();
   }
 
   /**
-   * Starts the cloud movement animation by moving it left continuously.
+   * Starts the animation loop for continuous cloud movement.
+   * Sets up interval for automatic leftward drift at 60fps.
    */
   animate() {
-    setInterval(() => {
-      this.moveLeft();
-    }, 1000 / 60); // ~60 FPS
+    setStoppableInterval(() => this.moveLeft(), 1000 / 60);
+  }
+
+  /**
+   * Moves cloud leftward when camera moves left, creating parallax effect.
+   * Used to simulate camera movement by moving background elements.
+   */
+  moveLeftWithCamera() {
+    this.x -= this.cameraSpeed;
+  }
+
+  /**
+   * Moves cloud rightward when camera moves right, creating parallax effect.
+   * Used to simulate camera movement by moving background elements.
+   */
+  moveRightWithCamera() {
+    this.x += this.cameraSpeed;
   }
 }
